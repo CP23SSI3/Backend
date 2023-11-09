f28283d0c90c8498e67351c0136b204a998ae4be
Cartoonydesu <toon.pi45@gmail.com>
2023-11-09 21:46:43 +0700

    GET workday and documents in Array

--- src/main/java/com/example/internhub/entities/Post.java
+++ src/main/java/com/example/internhub/entities/Post.java
@@ -85,4 +85,12 @@ public class Post extends ResponseData {
     @OneToMany(mappedBy = "post")
     private List<OpenPosition> openPositionList;
 
+    public String[] getWorkDay(){
+        return workDay.split(",");
+    }
+
+    public String[] getDocuments(){
+        return documents.split(",");
+    }
+
 }
