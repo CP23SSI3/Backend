f8105a16b84e88b06ad8c88eeb5e982c787fe0ba
Cartoonydesu <toon.pi45@gmail.com>
2023-12-11 17:07:44 +0700

    clean unused import + add EnumConstraint+Validator

--- src/main/java/com/example/internhub/validators/EnumWorkDayConstraint.java
+++ src/main/java/com/example/internhub/validators/EnumWorkDayConstraint.java
@@ -5,11 +5,11 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = EnumDocumentTypesValidator.class)
+@Constraint(validatedBy = EnumWorkDayValidator.class)
 @Target( {ElementType.METHOD, ElementType.FIELD} )
 @Retention(RetentionPolicy.RUNTIME)
-public @interface EnumDocumentTypesConstraint {
-    String message() default "Unknown documents type.";
+public @interface EnumWorkDayConstraint {
+    String message() default "Unknown work day.";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
