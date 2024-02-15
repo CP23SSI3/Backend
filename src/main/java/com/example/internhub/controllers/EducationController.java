43dac0e2b142791bde771f47800f5b50b0f4b1c2
Cartoonydesu <toon.pi45@gmail.com>
2024-02-15 16:22:12 +0700

    Add education entity service repo and controller + join column

---
+++ src/main/java/com/example/internhub/controllers/EducationController.java
@@ -0,0 +1,2 @@
+package com.example.internhub.controllers;public class EducationController {
+}
---
+++ src/main/java/com/example/internhub/repositories/EducationRepositories.java
@@ -0,0 +1,2 @@
+package com.example.internhub.repositories;public interface EducationRepositories {
+}
