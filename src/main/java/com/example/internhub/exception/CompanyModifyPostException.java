022897506c2cd0a5d7f4da2c7ba9312214a7e845
Cartoonydesu <toon.pi45@gmail.com>
2024-03-09 21:41:48 +0700

    Company can modify only own company's post

--- src/main/java/com/example/internhub/exception/CompanyModifyPostException.java
+++ src/main/java/com/example/internhub/exception/CompanyModifyPostException.java
@@ -2,6 +2,6 @@ package com.example.internhub.exception;
 
 public class CompanyModifyPostException extends  Exception{
     public CompanyModifyPostException() {
-        super ("Cannot create posts for the company that the user isn't in.");
+        super ("Cannot modify posts for the company that the user isn't in.");
     }
 }
--- src/main/java/com/example/internhub/services/MySQLPostService.java
+++ src/main/java/com/example/internhub/services/MySQLPostService.java
@@ -136,8 +136,6 @@ public class MySQLPostService implements PostService {
         DecodedJWT token = authService.decodeBearerToken(authorizationHeader);
         if (token.getSignature() != "ADMIN") {
             User user = userService.findUserByUserName(token.getSubject());
-            System.out.println(user.getCompany().getCompId());
-            System.out.println(checkCompId);
             if (!user.getCompany().getCompId().equals(checkCompId)) throw new CompanyModifyPostException();
         }
     }
@@ -145,6 +143,7 @@ public class MySQLPostService implements PostService {
     @Override
     public ResponseEntity deletePost(String postId, HttpServletRequest req, HttpServletResponse res) {
         try {
+            checkIfCompanyCanModifyPost(req, postId);
             getPostByPostId(postId);
             postRepository.deleteById(postId);
             return new ResponseEntity(new ResponseObject(200, "Delete post id " + postId + " successfully", null),
@@ -161,6 +160,7 @@ public class MySQLPostService implements PostService {
     @Override
     public ResponseEntity editPost(String postId, EditPostDTO editPostDTO, HttpServletRequest req, HttpServletResponse res) throws IllegalAccessException {
         try {
+            checkIfCompanyCanModifyPost(req, postId);
             Post post = getPostByPostId(postId);
             Post editPost = modelMapper.map(editPostDTO, Post.class);
             post.setClosedDate(editPost.getClosedDate());
