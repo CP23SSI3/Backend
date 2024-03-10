07fea2ab0ce7ac265121d23d5ba586cb340d1340
Cartoonydesu <toon.pi45@gmail.com>
2024-03-10 16:30:58 +0700

    modelMapper - UserPagination send res only neccessary field

---
+++ src/main/java/com/example/internhub/dtos/UserPaginationObject.java
@@ -0,0 +1,19 @@
+package com.example.internhub.dtos;
+
+import com.example.internhub.entities.Role;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class UserPaginationObject {
+    private LocalDateTime createdDate;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String phoneNumber;
+    private Role role;
+    private String userId;
+    private String username;
+}
