b4e3981702eafcb26f99039fa1a071f48f8d3bbb
Cartoonydesu <toon.pi45@gmail.com>
2024-03-29 17:20:56 +0700

    Sort user

--- src/main/java/com/example/internhub/repositories/UserRepository.java
+++ src/main/java/com/example/internhub/repositories/UserRepository.java
@@ -10,14 +10,4 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface UserRepository extends JpaRepository<User, String> {
     public User findByUsername(String username);
     public User findByEmail(String email);
-    @Query(value = "select * " +
-            "from (((users u left join languages l on l.userId = u.userId) " +
-            "left join skills s on u.userId = s.userId) " +
-            "left join educations edu on u.userId = edu.userId) " +
-            "left join experiences ex on u.userId = ex.userId " +
-            "where u.userId = :userId " +
-            "order by l.languageName asc, s.skillName asc, edu.startedYear desc, ex.startedYear desc"
-            ,
-    nativeQuery = true)
-    public User getUserByIdSort(@RequestParam("userId") String userId);
 }
